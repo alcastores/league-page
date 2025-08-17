c42c53af22350deffe50c1eb523396ab98bdde47
elcastores <arbeavers@gmail.com>
2025-08-17 13:40:26 -0400

    added back roster just incase

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -52,6 +52,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
+      "roster": 1,
       "ManagerID": "555698519320530944",
       "name": "Trust the Process",
       "tookOver": 2023, // (optional) used if a manager took over a team, delete this line or change to null otherwise
@@ -74,6 +75,7 @@ export const managers = [
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
+      "roster": 2,
       "ManagerID": "733888408518258688",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Shifty Grifter",
       "tookOver": 2023, // (optional) used if a manager took over a team, delete this line or change to null otherwise
@@ -96,6 +98,7 @@ export const managers = [
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
+      "roster": 3,
       "ManagerID": "979738150069219328",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Waddlin Tua Win",
       "tookOver": 2023, // (optional) used if a manager took over a team, delete this line or change to null otherwise
@@ -103,7 +106,7 @@ export const managers = [
       "bio": "Die-hard Baltimore fan, born and raised in Baltimore. Looking to compete every year.",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 1992, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "bal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
